34f5c607607d4de87bb4772e135b209e2185e05d
alisonsantosofc <alisonsantoso22@gmail.com>
2023-07-02 00:00:28 -0300

    build: update eslintrc

--- src/styles/styled.d.ts
+++ src/styles/styled.d.ts
@@ -5,6 +5,5 @@ import { lightTheme } from './themes';
 export type Theme = typeof lightTheme;
 
 declare module 'styled-components' {
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
   export interface DefaultTheme extends Theme {}
 }
